8cdcefe3b0d2fda17f87e67eced04b53dc7339da
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-09 14:29:13 +0000

    Made a menu and a word count extension method

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -10,19 +10,56 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a string");
-            string input = Console.ReadLine();
+            bool running = true;
 
-            //test ToLong, it will round since Long truncates with tryparse64
-            long value = input.ToLong();
-            Console.WriteLine(value);
+            while (running)
+            {
 
+
+                Console.WriteLine("Choose an extension method to try");
+                DisplayMenu();
+                string choice = Console.ReadLine();
+                char selected = choice[0];
+
+
+                switch (selected)
+                {
+                    case '1':
+                        Console.WriteLine("Enter a string");
+                        string input = Console.ReadLine();
+
+                        //test ToLong, it will round since Long truncates with tryparse64
+                        long value = input.ToLong();
+                        Console.WriteLine(value);
+                        break;
+                    case '2':
+                        Console.WriteLine("Enter a string");
+                        string input2 = Console.ReadLine();
+                        Console.WriteLine($"The number of words in the string is: {input2.WordCount()}");
+                        break;
+                    case 'Q':
+                        Console.WriteLine("Thank you for using my application");
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid option");
+                        break;
+
+                }
+
+                //leave a line after each run
+                Console.WriteLine();
+            }
             //stop from automatically closing
             Console.ReadLine();
 
         }
 
-       
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("(1) Convert a string to Long");
+            Console.WriteLine("(2) Count the number of words in a string");
+        }
     }
 
 
